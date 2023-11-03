ee8bdb379f33738a12133bd18643a8bab817e833
Marcos Iglesias <markkos89@gmail.com>
2023-11-02 22:24:39 -0300

    feat: intial commit, first updated version, styles not working properly

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,35 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
 
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiConfig, createConfig, configureChains, mainnet } from "wagmi";
+
+import { publicProvider } from "wagmi/providers/public";
+
+// Configure chains & providers with the Alchemy provider.
+// Two popular providers are Alchemy (alchemy.com) and Infura (infura.io)
+const { chains, publicClient } = configureChains([mainnet], [publicProvider()]);
+
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECTCLOUD_PROJECT_ID,
+  chains,
+});
+
+// Set up wagmi config
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+});
+
+// Pass config to React Context Provider
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
